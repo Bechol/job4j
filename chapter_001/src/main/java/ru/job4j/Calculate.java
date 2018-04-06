9c9080e5b9323a707a91ec15a86deec448b49ec0
Bechol <oleg071984@gmail.com>
2018-04-06 09:24:47 +0300

    Добавлен JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
+/**
+* Класс выводит в консоль строку "Hello World"
+* @author Oleg Bech (oleg071984@gmail.com)
+* @since 03.04.2018
+* @version 1.0
+*/
 
 public class Calculate{
+/**
+* Main
+* @param args - args.
+*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Oleg Bech (oleg071984@gmail.com)
+* @version $Id$
+* @since 06.04.2018
+*/
+package ru.job4j;
