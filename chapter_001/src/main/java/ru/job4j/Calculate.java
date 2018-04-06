aabe97e7f28eaa2efa55d2a7aca80be926ea8bdb
Bechol <oleg071984@gmail.com>
2018-04-06 11:04:03 +0300

    исправлены ошибки после аудита checkstyle

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -6,12 +6,12 @@ package ru.job4j;
 * @version 1.0
 */
 
-public class Calculate{
+public class Calculate {
 /**
 * Main
 * @param args - args.
 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	
