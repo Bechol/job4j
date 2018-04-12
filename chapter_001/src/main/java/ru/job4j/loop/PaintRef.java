243913749e02da591f783bb549692483ef7e7a2f
Bechol <oleg071984@gmail.com>
2018-04-13 00:48:00 +0300

    4.5. Рефакторинг кода.

---
+++ chapter_001/src/main/java/ru/job4j/loop/PaintRef.java
@@ -0,0 +1,63 @@
+package ru.job4j.loop;
+
+import java.util.function.BiPredicate;
+
+/**
+ * Рефакторинг кода класса Paint.
+ * @author Oleg Bech (oleg071984@gmail.com)
+ * @since 12.04.2018
+ * @version 1.0
+ */
+public class PaintRef {
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
+        StringBuilder screen = new StringBuilder();
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (predict.test(row, column)) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+    /**
+     * Method rightTrl.
+     * @param height высота пирамиды.
+     * @return строка с "правой" пирамидой.
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+    /**
+     * Method leftTrl.
+     * @param height высота пирамиды.
+     * @return строка с целой пирамидой.
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+    /**
+     * Method leftTrl.
+     * @param height высота пирамиды.
+     * @return строка с целой пирамидой.
+     */
+    public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+}
