a9072be5edafc701cf8e9a1fc551e6bbd74cb897
Bechol <oleg071984@gmail.com>
2018-04-12 19:03:11 +0300

    Тест для 3.4. Максимум из трех чисел

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -18,4 +18,10 @@ public class MaxTest {
         int result = maximum.maxInt(1, 2);
         assertThat(result, is(2));
     }
+    @Test
+    public void whenFirstLessThanOther() {
+        Max maxThree = new Max();
+        int result = maxThree.maxIntFromThree(1, 2, 3);
+        assertThat(result, is(3));
+    }
 }
