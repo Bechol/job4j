ffe04487ae76db7bad3862b7bb5a8968ef7361bc
Bechol <oleg071984@gmail.com>
2018-04-12 17:30:11 +0300

    Тест для 3.2. Максимум из двух чисел

---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ *
+ * @author Oleg Bech (oleg071984@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MaxTest {
+    @Test
+    public void whenFisrtLessThanSecond() {
+        Max maximum = new Max();
+        int result = maximum.maxInt(1, 2);
+        assertThat(result, is(2));
+    }
+}
