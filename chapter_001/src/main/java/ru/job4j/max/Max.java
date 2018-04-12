f46bdfa93fbec561fd3a1ab1501c819bf8ca2e8c
Bechol <oleg071984@gmail.com>
2018-04-12 17:29:34 +0300

    3.2. Максимум из двух чисел

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.max;
+/**
+ * Класс определяет  и возвращает максимальное из двух чисел.
+ * @author Oleg Bech (oleg071984@gmail.com)
+ * @since 09.04.2018
+ * @version 1.0
+ */
+public class Max {
+    /**
+     * Method maxInt.
+     * @param first первое число.
+     * @param second второе число.
+     * @return максимальное число из двух.
+     */
+    public int maxInt(int first, int second) {
+        return first > second ? first : second;
+    }
+}
