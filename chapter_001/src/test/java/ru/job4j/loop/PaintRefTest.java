051b0f914d9b96fdc348f06a87b1bb6ad74b2ff7
Bechol <oleg071984@gmail.com>
2018-04-13 00:48:17 +0300

    Тест 4.5. Рефакторинг кода.

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintRefTest.java
@@ -0,0 +1,74 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.util.StringJoiner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Oleg Bech (oleg071984@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PaintRefTest {
+    /**
+     * Test rightPyramid height = 4;
+     */
+    @Test
+    public void whenPyramid4Right() {
+        PaintRef paint = new PaintRef();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+    /**
+     * Test leftPyramid height = 4;
+     */
+    @Test
+    public void whenPyramid4Left() {
+        PaintRef paint = new PaintRef();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+    /**
+     * Test WholePyramid height = 3;
+     */
+    @Test
+    public void whenPyramidWhole() {
+        PaintRef paint = new PaintRef();
+        String rst = paint.pyramid(3);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("  ^  ")
+                                .add(" ^^^ ")
+                                .add("^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
