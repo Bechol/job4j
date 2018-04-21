9c56f7f1b0c36b1d8043170b0bba64d0540d0ba6
Bechol <oleg071984@gmail.com>
2018-04-21 14:03:26 +0300

    5.5. Слова начинается с ...

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,8 +1,16 @@
 package ru.job4j.array;
-
+/**
+ * 5.5. Слова начинается с ....
+ * @author Oleg Bech (oleg071984@gmail.com)
+ * @since 12.04.2018
+ * @version 1.0
+ */
 public class ArrayChar {
     private char[] data;
-
+    /**
+     * Конструктор класса. Создает массив символов из строки.
+     * @param line строка.
+     */
     public ArrayChar(String line) {
         this.data = line.toCharArray();
     }
@@ -14,14 +22,11 @@ public class ArrayChar {
     public boolean startWith(String prefix) {
         boolean result = true;
         char[] value = prefix.toCharArray();
-        int valueArrayLength = value.length;
-        for (int i = 0; i <= (data.length - value.length); i++) {
+        for (int i = 0; i < value.length; i++) {
             if (data[i] != value[i]) {
                 result = false;
             }
         }
-
-        // проверить. что массив data имеет первые элементы одинаковые с value
         return result;
     }
 }
