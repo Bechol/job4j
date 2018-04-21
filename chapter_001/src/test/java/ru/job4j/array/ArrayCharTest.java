3386471ba0b0b73dc3aa1a638e531c3a1aa4fe65
Bechol <oleg071984@gmail.com>
2018-04-21 14:03:47 +0300

    Тесты для 5.5. Слова начинается с ...

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ *
+ * @author Oleg Bech (oleg071984@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayCharTest {
+    /**
+     * Test фрагмент содержится в слове.
+     */
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+    /**
+     * Test фрагмент не содержится в слове.
+     */
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+
+}
