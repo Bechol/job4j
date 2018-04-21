21dc3441dce6de334b3316e23465357b27fba003
Bechol <oleg071984@gmail.com>
2018-04-21 13:50:28 +0300

    ...

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        int valueArrayLength = value.length;
+        for (int i = 0; i <= (data.length - value.length); i++) {
+            if (data[i] != value[i]) {
+                result = false;
+            }
+        }
+
+        // проверить. что массив data имеет первые элементы одинаковые с value
+        return result;
+    }
+}
