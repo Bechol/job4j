a7c3fde46ae4e09862a3798d3d2c6c8ffb3e2141
Bechol <oleg071984@gmail.com>
2018-05-02 21:46:06 +0300

    тесты для индивидуального задания

---
+++ chapter_001/src/test/java/ru/job4j/array/NewArraySortTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ *
+ * @author Oleg Bech (oleg071984@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class NewArraySortTest {
+    /**
+     * Test сравнение двух массивов.
+     */
+    @Test
+    public void whenFirstArrayIs5ElementsAndSecondArrayIs5ElementMakeThirdArray10ElementsAndSort() {
+        int[] firstArray = {34, 56, 38, 378, 89};
+        int[] secondArray = {45, 238, 12, 87, 44};
+        int[] expected = {12, 34, 38, 44, 45, 56, 87, 89, 238, 378};
+        NewArraySort newArray = new NewArraySort();
+        int[] resultArray = newArray.thirdArrSort(firstArray, secondArray);
+        assertThat(resultArray, is(expected));
+
+    }
+    /**
+     * Test сравнение двух cумм элементов массивов.
+     */
+    @Test
+    public void whenSumOfTwoElementsOfArrayIs79ThenSumOfElementOfResultArrayIs79() {
+        int[] firstArray = {34, 56, 38, 378, 89};
+        int[] secondArray = {45, 238, 12, 87, 44};
+        int expextedSum = 79;
+        NewArraySort newArray = new NewArraySort();
+        int[] resultArray = newArray.thirdArrSort(firstArray, secondArray);
+        int resultSum = resultArray[1] + resultArray[4];
+        assertThat(resultSum, is(expextedSum));
+
+    }
+}
