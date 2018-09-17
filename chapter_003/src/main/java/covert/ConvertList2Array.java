691ab4a07bd51567cc85623274ab53cab7f8294e
Bechol <oleg071984@gmail.com>
2018-09-17 21:49:08 +0300

    3. Конвертация ArrayList в двухмерный массив.

---
+++ chapter_003/src/main/java/covert/ConvertList2Array.java
@@ -0,0 +1,36 @@
+package covert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс ConvertList2Array.
+ * Реализация метода преобразования ArrayList в двумерный массив.
+ * @author Oleg Bech (oleg071984@gmail.com)
+ * @since 17.09.2018
+ * @version 1.0
+ */
+public class ConvertList2Array {
+    /**
+     * Метод toArray.
+     * Преобразование ArrayList в двумерный массив.
+     * @param list ArrayList.
+     * @param rows количество строк.
+     * @return двумерный массив.
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        int cells = list.size() % rows != 0 ? list.size() / rows + 1 : list.size() / rows;
+        int[][] array = new int[rows][cells];
+        int rowIndex = 0;
+        int cellIndex = 0;
+            for (Integer arrItem : list) {
+                array[rowIndex][cellIndex] = arrItem;
+                cellIndex++;
+                    if (cellIndex == cells) {
+                        rowIndex++;
+                        cellIndex = 0;
+                    }
+            }
+        return array;
+    }
+}
