ba0bff5a577d24b8d07db4426bca1b9d93595768
Bechol <oleg071984@gmail.com>
2018-10-28 14:46:16 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

--- chapter_005/src/main/java/collectionspro/iterator/Converter.java
+++ chapter_005/src/main/java/collectionspro/iterator/Converter.java
@@ -1,18 +1,50 @@
 package collectionspro.iterator;
 
-import java.util.Iterator;
-
+import java.util.*;
+/**
+ * Класс Converter.
+ * Итератор итераторов.
+ * @author Oleg Bech (oleg071984@gmail.com).
+ * @since 27.10.2018.
+ * @version 1.0.
+ */
 public class Converter {
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
         return new Iterator<Integer>() {
+
+            private Iterator<Integer> iterator;
+
+            private void check() {
+                if (iterator == null && it.hasNext()) {
+                    iterator = it.next();
+                }
+            }
+
             @Override
             public boolean hasNext() {
-                return false;
+                check();
+                if (iterator == null) {
+                    return false;
+                }
+                if (iterator.hasNext()) {
+                    return true;
+                }
+                if (it.hasNext()) {
+                    iterator = it.next();
+                }
+                return iterator.hasNext();
             }
 
             @Override
             public Integer next() {
-                return null;
+                check();
+                if (iterator == null) {
+                    throw new NoSuchElementException();
+                }
+                if (!iterator.hasNext() && it.hasNext()) {
+                    iterator = it.next();
+                }
+                return iterator.next();
             }
         };
     }
